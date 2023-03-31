a9dd2b99b0c765b66999a33f36eb2ad6a0b4e5f9
Prashantha S P <prashanth.sp98@gmail.com>
2023-04-01 01:14:47 +0530

    - Added tmux config - bash script to setup required tools

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+for i in git tmux xclip vim gcc;
+do
+
+    which $i 2>&1 >/dev/null
+    TOOL_IS_AVAILABLE=$?
+
+    if [ $TOOL_IS_AVAILABLE -ne 0 ]; then
+        echo "Installing $i"
+        sudo apt install -y $i
+    fi
+
+done
+
+# install nvim build dependencies
+sudo apt-get install ninja-build gettext libtool-bin cmake g++ pkg-config unzip curl
+git clone https://github.com/neovim/neovim ~/nvim_source
+
+git clone https://github.com/prashanthsp6498/nvim-config.git ~/.config/nvim
+
+ln -s .tmux.conf ~/.tmux.conf
+
